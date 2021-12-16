6a8bf3520ed88b8055f3ae8bf5ebd4b27780dbd3
Mohammed Dilshad <dilshadt101@gmail.com>
2021-12-16 17:48:06 +0530

    add delete view

--- base/views.py
+++ base/views.py
@@ -37,10 +37,17 @@ def update_room(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
     if request.method == 'POST':
-        form = RoomForm(request.POST, room)
+        form = RoomForm(request.POST, instance=room)
         if form.is_valid():
             form.save()
             return redirect('home')
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
