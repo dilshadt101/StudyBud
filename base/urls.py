adf221687d599fd59383af2927dda524cfbac927
Mohammed Dilshad <dilshadt101@gmail.com>
2021-12-22 17:29:42 +0530

    add registration for user

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_page, name='register'),
     path('', views.home, name='home'),
     path('room/<str:pk>', views.room, name='room'),
     path('create-room/', views.create_room, name='create-room'),
